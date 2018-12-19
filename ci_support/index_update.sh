9792b407b5dcaffb8cfb6f70874e0e840442e330
Luciano Gigantelli Zago <ra182835@students.ic.unicamp.br>
2018-12-19 20:36:29 -0200

    Update repodata.json in x86

---
+++ ci_support/index_update.sh
@@ -0,0 +1,32 @@
+# Install miniconda
+sudo apt-get update
+
+ARCH=`uname -m`
+echo "ARCH: $ARCH"
+
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-$ARCH.sh -O miniconda.sh;
+else
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-$ARCH.sh -O miniconda.sh;
+fi
+
+chmod +x miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+echo 'PATH =' $PATH
+conda config --set always_yes yes
+conda info -a
+
+
+# Install conda build
+conda install conda-build
+
+# Install lftp
+sudo apt-get install lftp
+
+# Get the files
+wget -P $HOME/miniconda/repo https://oplab9.parqtec.unicamp.br/pub/ppc64el/power-ai/linux-ppc64le/
+
+# Generate conda index
+cd $HOME/miniconda/repo
+conda index

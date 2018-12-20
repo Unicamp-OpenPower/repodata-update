4332d9351decdc02bfe37191184be792042ab43b
Luciano Gigantelli Zago <ra182835@students.ic.unicamp.br>
2018-12-20 09:18:39 -0200

    Fix upload script

--- ci_support/ftp_upload.sh
+++ ci_support/ftp_upload.sh
@@ -3,21 +3,18 @@
 set -e
 
 FTP_HOST='oplab9.parqtec.unicamp.br'
-LOCALPATH=$HOME/miniconda/repo/linux-ppc64le
+LOCALPATH=$HOME/miniconda/repo
 REMOTEPATH='/ppc64el/power-ai/linux-ppc64le'
 
 echo "Starting upload..."
 
 # Upload json file from LOCALPATH to REMOTEPATH
-  lftp -e "
-  set dns:order "inet";
-  set net:timeout 15;
-  set net:reconnect-interval-base 5;
-  set net:max-retries 2;
-  open ftp://$FTP_HOST;
-  user $FTP_USER $FTP_PASSWORD;
-  mirror -R --continue --reverse --no-empty-dirs --no-perms -i repodata.$ $LOCALPATH $REMOTEPATH;
-  bye;
-  "
+lftp -d -u $FTP_USER,$FTP_PASSWORD $FTP_HOST << EOF
+set dns:order inet
+set net:timeout 15
+set net:reconnect-interval-base 5
+set net:max-retries 2
+mirror -R --continue --reverse --no-empty-dirs --no-perms -i repodata* $LOCALPATH $REMOTEPATH
+EOF
 
-  echo "Finishing upload..."
+echo "Finishing upload..."

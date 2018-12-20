564b77de51710c483bd552cf5906333bd6c6ff32
Luciano Gigantelli Zago <ra182835@students.ic.unicamp.br>
2018-12-20 00:25:08 -0200

    Change include regex

--- ci_support/ftp_upload.sh
+++ ci_support/ftp_upload.sh
@@ -7,13 +7,13 @@ LOCALPATH=$HOME/miniconda/repo/linux-ppc64le
 REMOTEPATH='/ppc64el/power-ai/linux-ppc64le'
 
 # Upload json file from LOCALPATH to REMOTEPATH
-lftp -e "
-set dns:order "inet"
-set net:timeout 15
-set net:reconnect-interval-base 5
-set net:max-retries 2
-open ftp://$FTP_HOST
-user $FTP_USER $FTP_PASSWORD
-mirror -R --continue --reverse --no-empty-dirs --no-perms -i \.repodata.$ $LOCALPATH $REMOTEPATH
-bye
-"
+  lftp -e "
+  set dns:order "inet"
+  set net:timeout 15
+  set net:reconnect-interval-base 5
+  set net:max-retries 2
+  open ftp://$FTP_HOST
+  user $FTP_USER $FTP_PASSWORD
+  mirror -R --continue --reverse --no-empty-dirs --no-perms -i repodata.$ $LOCALPATH $REMOTEPATH
+  bye
+  "

8e4c1100814a2a0990532b60cc1521e718b95588
Luciano Gigantelli Zago <ra182835@students.ic.unicamp.br>
2018-12-19 23:40:50 -0200

    Update conda index parameters

--- ci_support/ftp_upload.sh
+++ ci_support/ftp_upload.sh
@@ -12,10 +12,8 @@ set dns:order "inet"
 set net:timeout 15
 set net:reconnect-interval-base 5
 set net:max-retries 2
-set xfer:use-temp-file yes
-set xfer:temp-file-name *.tmp
 open ftp://$FTP_HOST
 user $FTP_USER $FTP_PASSWORD
-mirror -R --continue --reverse --no-empty-dirs --no-perms -i \.json$ $LOCALPATH $REMOTEPATH
+mirror -R --continue --reverse --no-empty-dirs --no-perms -i \.repodata.$ $LOCALPATH $REMOTEPATH
 bye
 "
